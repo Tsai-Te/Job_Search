346184f610389c015a8c928f21e08ca578a86d02
Te Cai <tsai_te@Tes-MacBook-Air.fios-router.home>
2019-09-19 23:40:17 -0400

    service updates

--- mvc/src/main/java/com/te/service/RegionService.java
+++ mvc/src/main/java/com/te/service/RegionService.java
@@ -19,32 +19,50 @@ public class RegionService {
     }
 
     @Transactional
-    public List<Region> findByCity(String city){
+    public List<Region> findByCity(String city) throws Exception{
+        if(city==null){
+            throw new NullPointerException("");
+        }
         return regionRepository.findByCity(city);
     }
 
     @Transactional
-    public List<Region> findByState(String state){
+    public List<Region> findByState(String state) throws Exception{
+        if(state==null){
+            throw new NullPointerException("");
+        }
         return regionRepository.findByState(state);
     }
 
     @Transactional
-    public List<Region> findByZipCode(int zipCode){
+    public List<Region> findByZipCode(Integer zipCode) throws Exception{
+        if(zipCode==null){
+            throw new NullPointerException("");
+        }
         return regionRepository.findByZipCode(zipCode);
     }
 
     @Transactional
-    public Region generateRegion(Region region){
+    public Region generateRegion(Region region) throws Exception{
+        if(region==null){
+            throw new NullPointerException("");
+        }
         return regionRepository.save(region);
     }
 
     @Transactional
-    public Region findById(Long id){
+    public Region findById(Long id) throws Exception{
+        if(id==null){
+            throw new NullPointerException("");
+        }
         return regionRepository.findById(id).get();
     }
 
     @Transactional
-    public Region findByIdEager(Long id){
+    public Region findByIdEager(Long id) throws Exception{
+        if(id==null){
+            throw new NullPointerException("");
+        }
         Region result=regionRepository.findByIdEager(id).get();
         return result;
     }
